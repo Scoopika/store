69f6aad55bf4e75ae011d4f002bb465db53a3a03
Kais Radwan <kais.radwan.personal@gmail.com>
2024-05-01 11:19:20 -0400

    Init

---
+++ main.ts
@@ -0,0 +1,222 @@
+import * as types from "npm:@scoopika/types";
+import { Hono, Context } from "hono";
+import { cors } from "hono/cors";
+
+const kv = await Deno.openKv();
+const app = new Hono();
+
+app.onError((err, c) => {
+  return c.json({
+    success: false,
+    error: `${err}`
+  }, 500)
+})
+
+app.use("/*", cors({
+  origin: ["*"]
+}))
+
+// <TOKEN_MIDDLEWARE>
+
+app.get("/session/:id", async (c: Context) => {
+
+  const id = c.req.param("id");
+
+  if (!id || typeof id !== "string") {
+    return c.json({
+      success: false,
+      error: "Session id not provided"
+    }, 400);
+  }
+
+  const session = await kv.get<types.StoreSession>(["sessions", id]);
+
+  if (!session.value) {
+    return c.json({
+      success: false,
+      error: "Session not found"
+    }, 404);
+  }
+
+  return c.json({
+    success: true,
+    session: session.value
+  }, 200);
+})
+
+app.post("/session/:id", async (c) => {
+
+  const id = c.req.param("id");
+
+  const exist = await kv.get<types.StoreSession>(["sessions", id]);
+
+  if (exist.value) {
+    return c.json({
+      success: false,
+      error: "Session already exist"
+    }, 403);
+  }
+
+  const session = await c.req.json<types.StoreSession>();
+
+  if (!session || typeof session !== "object") {
+    return c.json({
+      success: false,
+      error: "Invalid session data"
+    }, 400);
+  }
+
+  await kv.set(["sessions", id], session);
+  await kv.set(["history", id], []);
+
+  if (session.user_id) {
+    const exist_user = await kv.get<string[]>(["user_sessions", session.user_id]);
+    let sessionsIDs = [id];
+
+    if (exist_user.value) {
+      sessionsIDs = [...exist_user.value, id];
+    }
+
+    await kv.set(
+      ["user_sessions", session.user_id],
+      sessionsIDs
+    );
+  }
+
+  return c.json({
+    success: true
+  }, 200);
+})
+
+app.delete("/session/:id", async (c) => {
+  const id = c.req.param("id");
+
+  const exist = await kv.get<types.StoreSession>(["sessions", id]);
+
+  if (!exist.value) {
+    return c.json({
+      success: false,
+      error: "Session not found"
+    }, 404);
+  }
+
+  await kv.delete(["sessions", id]);
+  await kv.delete(["history", id]);
+
+  const exist_user = exist.value.user_id;
+
+  if (!exist_user) {
+    return c.json({
+      success: true
+    });
+  }
+
+  const user_sessions = await kv.get<string[]>(["user_sessions", exist_user]);
+  if (!user_sessions.value) {
+    return c.json({
+      success: true
+    })
+  }
+
+  const wanted_sessions = user_sessions.value.filter(s => s !== id);
+  await kv.set(["user_sessions", exist_user], wanted_sessions);
+
+  return c.json({
+    success: true
+  })
+})
+
+app.put("/session/:id", async (c) => {
+  const id = c.req.param("id");
+
+  const newData = await c.req.json<types.StoreSession>();
+  const session = await kv.get<types.StoreSession>(["sessions", id]);
+
+  if (!session.value) {
+    return c.json({
+      success: false,
+      error: "Session not found"
+    }, 404);
+  }
+
+  newData.id = session.value.id;
+  newData.user_id = session.value.user_id;
+
+  await kv.set(["sessions", id], {
+    ...session.value,
+    ...newData
+  })
+
+  return c.json({
+    success: true
+  })
+})
+
+app.get("/user_sessions/:id", async (c) => {
+  const id = c.req.param("id");
+
+  const sessions = await kv.get(["user_sessions", id]);
+
+  if (!sessions.value) {
+    return c.json({
+      success: true,
+      sessions: []
+    });
+  }
+
+  return c.json({
+    success: true,
+    sessions: sessions.value
+  })
+})
+
+app.get("history/:id", async (c) => {
+  const id = c.req.param("id");
+
+  const exist = await kv.get<types.LLMHistory>(["history", id]);
+
+  if (!exist.value) {
+    return c.json({
+      success: false,
+      error: "Session not found"
+    }, 404);
+  }
+
+  return c.json({
+    success: true,
+    history: exist.value
+  })
+})
+
+app.post("/history/:id", async (c) => {
+  const id = c.req.param("id");
+
+  const exist = await kv.get<types.LLMHistory[]>(["history", id]);
+
+  if (!exist.value) {
+    return c.json({
+      success: false,
+      error: "Session not found"
+    }, 404);
+  }
+
+  const { history } = (await c.req.json<{history: types.LLMHistory[]}>()) || {
+    history: []
+  };
+
+  if (!history) {
+    return c.json({
+      success: false,
+      error: "Invalid request body"
+    }, 400);
+  }
+
+  const newHistory: types.LLMHistory[] = [...exist.value, ...history];
+  await kv.set(["history", id], newHistory);
+
+  return c.json({
+    success: true
+  })
+})
+
+Deno.serve(app.fetch);
---
+++ main_test.ts
@@ -0,0 +1,112 @@
+import { StoreSession } from "npm:@scoopika/types";
+import { assertEquals } from "jsr:@std/assert";
+
+const URL = "http://localhost:8000";
+const sessionID = crypto.randomUUID();
+const userID = crypto.randomUUID();
+
+Deno.test("Create session", async () => {
+  
+  const mySession: StoreSession = {
+    id: sessionID,
+    user_id: userID,
+    saved_prompts: {},
+    user_name: "User",
+  }
+
+  const res = await fetch(`${URL}/session/${mySession.id}`, {
+    method: "POST",
+    body: JSON.stringify({
+      ...mySession
+    })
+  })
+
+  const data = await res.json();
+  assertEquals(data.success, true);
+})
+
+Deno.test("Get session", async () => {
+  const res = await fetch(`${URL}/session/${sessionID}`);
+  const data = await res.json();
+
+  assertEquals(data.success, true);
+  assertEquals(data.session.id, sessionID);
+  assertEquals(data.session.user_id, userID);
+})
+
+Deno.test("List user sessions", async () => {
+  const res = await fetch(`${URL}/user_sessions/${userID}`);
+  const data = await res.json();
+
+  assertEquals(data.success, true);
+  assertEquals(typeof data.sessions, "object");
+  assertEquals(data.sessions?.length, 1);
+})
+
+Deno.test("Update session", async () => {
+  const res = await fetch(`${URL}/session/${sessionID}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      user_name: "updated_user"
+    })
+  })
+  const data = await res.json();
+
+  assertEquals(data.success, true);
+})
+
+Deno.test("Read updated session", async () => {
+  const res = await fetch(`${URL}/session/${sessionID}`);
+  const data = await res.json();
+
+  assertEquals(data.success, true);
+  assertEquals(data.session?.id, sessionID);
+  assertEquals(data.session?.user_name, "updated_user");
+})
+
+Deno.test("Push to history", async () => {
+  const res = await fetch(`${URL}/history/${sessionID}`, {
+    method: "POST",
+    body: JSON.stringify({
+      history: [
+        {role: "user", content: "Hello"}
+      ]
+    })
+  })
+
+  const data = await res.json();
+
+  assertEquals(data.success, true);
+})
+
+Deno.test("Read history", async () => {
+  const res = await fetch(`${URL}/history/${sessionID}`);
+  const data = await res.json();
+
+  assertEquals(data.success, true);
+  assertEquals(data.history?.length, 1);
+})
+
+Deno.test("Delete session", async () => {
+  const res = await fetch(`${URL}/session/${sessionID}`, {
+    method: "DELETE",
+  });
+
+  const data = await res.json();
+  assertEquals(data.success, true);
+})
+
+Deno.test("List empty user sessions", async () => {
+  const res = await fetch(`${URL}/user_sessions/${userID}`);
+  const data = await res.json();
+
+  assertEquals(data.success, true);
+  assertEquals(data.sessions?.length, 0);
+})
+
+Deno.test("Get deleted session", async () => {
+  const res = await fetch(`${URL}/session/${sessionID}`);
+  const data = await res.json();
+
+  assertEquals(data.success, false);
+})
